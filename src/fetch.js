554913124d6073046a1245b4250190e9e939ba4f
Arpit <arpit2911011@gmail.com>
2023-01-25 14:40:34 +0530

    Date Issue

--- src/fetch.js
+++ src/fetch.js
@@ -4,18 +4,27 @@ import format from 'date-fns/format';
 import 'antd/dist/reset.css';
 const {RangePicker} =DatePicker;
 function Dat() {
-    const[start,setstart] =useState('');
+    const[start,setstart] =useState(new Date());
     
-    const[end,setend] =useState('');
-   console.log(start);
-   console.log(end);
-    const url=`http://go-dev.greedygame.com/v3/dummy/report?startDate=${start}&endDate=${end}`;
+    const[end,setend] =useState(new Date());
+    
+  //  console.log(start.getFullYear() + '-' + 0  + '-' + start.getDate());
+  //  console.log(end);
+
+   
+   
+    // const url=`http://go-dev.greedygame.com/v3/dummy/report?startDate=${start.getFullYear() + '-' + 0  + '-' + start.getDate()}&endDate=${end.getFullYear() + '-' + 0  + '-' + end.getDate()}`;
   
-    async function fetchdata(){
-        const response= await fetch(url);
+    async function fetchdata(start, end){
+     
+      var mons = start.getMonth() + 1;
+   var mone = end.getMonth() + 1;
+
+        const response= await fetch(`http://go-dev.greedygame.com/v3/dummy/report?startDate=${start.getFullYear() + '-' + 0 + mons + '-' + start.getDate()}&endDate=${end.getFullYear() + '-' + 0 + mone + '-' + end.getDate()}`);
         const jsondata= await response.json();
 
-        // console.log(jsondata);
+
+        console.log(jsondata);
     }
     useEffect(() => {
         fetchdata();
@@ -24,13 +33,14 @@ function Dat() {
         <div>
             <div>
                 <RangePicker
+                dateFormat = "dd/mm/yyyy"
                 onChange={(values)=>{
-                    setstart(values[0]);
-                    setend(values[1]);
+                    setstart(new Date(values[0]));
+                    setend(new Date(values[1]));
                 }}
                 />
             </div>
-         <button onClick={fetchdata}>Apply</button>
+         <button onClick={() => {fetchdata(start,end)}}>Apply</button>
         </div>
       );
 }
